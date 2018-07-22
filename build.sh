323950877be436f645f49f2e08255d1c75ce3a4b
dkati <dkatikaridis@gmail.com>
2018-07-22 16:01:56 +0300

    build.sh:Remove ionice and make pure mka

--- build.sh
+++ build.sh
@@ -3,17 +3,19 @@
 source build/envsetup.sh
 lunch lineage_jfltexx-userdebug
 echo "Building..."
-time schedtool -B -n 1 -e ionice -n 1 mka halium-boot -j"$CPU_NUM" "$@"
+mka halium-boot
 if [ "$?" == 0 ]; then
-		echo "HALIUM-BOOT DONE."
-	else
-		echo "HALIUM-BOOT FAILED."
+	echo "HALIUM-BOOT DONE."
+else
+	echo "HALIUM-BOOT FAILED."
+	exit -1;
 fi
 echo ""
-time schedtool -B -n 1 -e ionice -n 1 mka systemimage -j"$CPU_NUM" "$@"
+mka systemimage
 if [ "$?" == 0 ]; then
-		echo "SYSTEMIMAGE DONE."
-	else
-		echo "SYSTEMIMAGE FAILED."
+	echo "SYSTEMIMAGE DONE."
+else
+	echo "SYSTEMIMAGE FAILED."
+	exit -1;
 fi
 echo ""
